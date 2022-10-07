46b7100ef4d4ba6bab7de1efdd2f06b22cc1e900
Antonio <antoniotropiano@gmail.com>
2022-10-07 22:32:59 +0300

    modify build

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pippip install --force-reinstall -U setuptools
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
 
 poetry install
 
