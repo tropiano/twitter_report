56b05174707c6a17db2fe307460bc6e5ba8d850f
Antonio <antoniotropiano@gmail.com>
2022-10-07 22:43:19 +0300

    modify build again (setuptools after poetry install)

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 # exit on error
 set -o errexit
 
+poetry install
 pip install --upgrade pip
 pip install --force-reinstall -U setuptools
-apt-get install --reinstall python-pkg-resources
-
-poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
