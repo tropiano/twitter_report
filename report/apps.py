6a47f6881b088ce7a00177930288ad7d35ce59d2
Antonio <antoniotropiano@gmail.com>
2022-10-11 18:24:48 +0300

    activate the scheduler

--- report/apps.py
+++ report/apps.py
@@ -5,6 +5,6 @@ class ReportConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'report'
 
-#    def ready(self):
-#        from report.scheduler import scheduler
-#        scheduler.start()
+    def ready(self):
+        from report.scheduler import scheduler
+        scheduler.start()
